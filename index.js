a80bb41e3dc018a651b0a4052e12d9780441fee0
malyn.gamboa@student.passerellesnumeriques.org <malyn.gamboa@student.passerellesnumeriques.org>
2019-09-24 17:56:49 +0800

    balik na pud ta ani

--- index.js
+++ index.js
@@ -1,28 +1,11 @@
 var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var port = process.env.PORT || 8000;
+var http = require('http').createServer(app);
+var ports = process.env.PORT || 3000
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function (socket) {
-  socket.on('login', function(username){
-    io.emit('login', username)
-  })
-
-  socket.on('chat message', function (msg) {
-    io.emit('chat message', msg);
-    console.log('a user connected ' + socket.client);
-  });
-
-  socket.on('typing', (data) => {
-    socket.broadcast.emit('typing', { username: socket.username })
-    })
-});
-
-
-http.listen(port, function () {
-  console.log('listening on *:' + port);
+http.listen(ports, function(){
+  console.log('listening on *: '+ports);
 });
